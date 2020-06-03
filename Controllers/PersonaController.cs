eec2a12ffef455ee956ed462bbd5dc59e3a885a5
Jaime TAMAYO CONDE <jtamayo@upt.pe>
2020-06-03 17:08:11 -0500

    asdas

--- Controllers/PersonaController.cs
+++ Controllers/PersonaController.cs
@@ -10,8 +10,6 @@ namespace Lab2_MVC_Tamayo.Controllers
     {
         //si o si lo hizo
         //hcaer una cambio para personaasdasdasdasdasd
-        List<ClsPersona> objLista = new List<ClsPersona>();
-        // GET: Persona
         public ActionResult Index()
         {
             //Instanciar para el objeto 1
